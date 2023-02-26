95e4c969669837951d59ca71fe0e03171e1085ba
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-02-26 17:01:40 -0300

    [client] Añadimos el tablero kanban como hijo de home

--- client/src/routes/Index.jsx
+++ client/src/routes/Index.jsx
@@ -3,13 +3,20 @@ import {
 	RouterProvider } from "react-router-dom"
 import ErrorPage from "../views/ErrorPage";
 import Home from "../views/Home";
+import KanbanBoard from "../views/KanbanBoard";
 
 const Index = () => {
 	const router = createBrowserRouter([
 		{
 			path:"/",
 			element: <Home />,
-			errorElement: <ErrorPage />
+			errorElement: <ErrorPage />,
+			children: [
+				{
+					path:"/",
+					element: <KanbanBoard />		
+				}
+			]
 		}
 	])
 
