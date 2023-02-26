ee197dc189484e65305cc698f2524e8ecbef5a0f
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-02-26 18:07:19 -0300

    [client] Implementa tarjetas kanban

--- client/src/views/KanbanBoard.jsx
+++ client/src/views/KanbanBoard.jsx
@@ -3,8 +3,18 @@ import {
 	Row,
 	Col
 } from "react-bootstrap"
+import CardKanban from "../components/CardKanban";
+import {
+	valuesBacklog,
+	valuesInProgress,
+	valuesCompleted
+} from "../test/valuesKanban.test";
 
 const KanbanBoard = () => {
+	const backlog = valuesBacklog;
+	const inProgress = valuesInProgress;
+	const completed = valuesCompleted;
+
 	return (
 		<>
 		<Container>
@@ -16,8 +26,12 @@ const KanbanBoard = () => {
 						</div>
 					</Row>
 					<Row>
-						<div className="border-top border-dark">
-							Hola
+						<div className="border-top border-dark pb-2">
+							{
+								backlog.map((item, index) =>{
+									return <CardKanban key={index} values={item} />
+								})
+							}
 						</div>
 					</Row>
 				</Col>
@@ -29,8 +43,12 @@ const KanbanBoard = () => {
 						</div>
 					</Row>
 					<Row>
-						<div className="border-top border-dark">
-							Hola
+						<div className="border-top border-dark pb-2">
+							{
+								inProgress.map((item, index) =>{
+									return <CardKanban key={index} values={item} />
+								})
+							}
 						</div>
 					</Row>
 				</Col>
@@ -42,8 +60,12 @@ const KanbanBoard = () => {
 						</div>
 					</Row>
 					<Row>
-						<div className="border-top border-dark">
-							Hola
+						<div className="border-top border-dark pb-2">
+							{
+								completed.map((item, index) =>{
+									return <CardKanban key={index} values={item} />
+								})
+							}
 						</div>
 					</Row>
 				</Col>
