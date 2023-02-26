40873e249882f127c91f374f7402ab388773040d
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-02-26 17:01:07 -0300

    [client] Despliega los hijo de home

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,8 +1,14 @@
+import { Outlet } from "react-router-dom"
+
 const Home = () => {
 	return(
 		<>
 		<div className="text-center fw-bolder">
 			<h1>Project Manager</h1>
+		</div>
+		<div>
+			{/* Renderiza los hijos indicados en el Index */}
+			<Outlet />
 		</div>
 		</>
 	);
