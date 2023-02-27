6afc2e0ca6618fa230851d4ce97ffe9d50a37ec5
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-02-27 02:48:24 -0300

    [client] Manejador de envio de datos a la DB

--- client/src/views/NewProject.view.jsx
+++ client/src/views/NewProject.view.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { 
 	Container, 
 	Col, 
@@ -6,8 +7,38 @@ import {
 	Card, 
 	Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const NewProject = () => {
+	const [formValid, setFormValid] = useState(false);
+	const [title, setTitle] = useState("");
+	const [dueDate, setDueDate] = useState("");
+	const navigate = useNavigate();
+
+	// * Manejador de envio de datos
+	const handlerSubmit = (event) => {
+		const data = {
+			title, 
+			dueDate,
+			state:"bg-warning",
+			status:"Start Project",
+		};
+		event.preventDefault();
+
+		// * Validamos nuestros datos antes de enviar
+		if(title && dueDate !== "")
+		// * Se envia los datos al post
+			axios.post(
+				"http://localhost:8000/api/setProject", 
+				data)
+				.then(res => navigate("/")) // * Si la respuesta es exitosa retorna la "home"
+				.catch(err => console.log(err))
+		else
+			setFormValid(true)
+	}
+
+
 	return (
 		<>
 			<Container>
@@ -22,22 +53,35 @@ const NewProject = () => {
 						<Card>
 							<Card.Header>Plan a new project</Card.Header>
 							<Card.Body className="p-5">
-								<Form>
+								<Form onSubmit={handlerSubmit} noValidate validated={formValid}>
 									<Form.Group as={Row} controlId="formNewProject" className="mb-5">
 										<Form.Label column sm={2}>Project</Form.Label>
 										<Col sm={10}>
-										<Form.Control type="text" placeholder="Name of project" /> 
+										<Form.Control 
+											required
+											minLength={3}
+											type="text" placeholder="Name of project"
+											onChange={e => setTitle(e.target.value)} />
+										<Form.Control.Feedback type="invalid">
+											Please set a name
+										</Form.Control.Feedback>
 										</Col>
 									</Form.Group>
 
 									<Form.Group as={Row} controlId="formNewProject" className="mb-5">
 										<Form.Label column sm={2}>Due date</Form.Label>
 										<Col sm={10}>
-										<Form.Control type="date" /> 
+										<Form.Control 
+											required
+											type="date" 
+											onChange={e => setDueDate(e.target.value)} />
+										<Form.Control.Feedback type="invalid">
+											Please select a day
+										</Form.Control.Feedback>
 										</Col>
 									</Form.Group>
 									<Row>
-										<Button>Plan Project</Button>
+										<Button type="submit">Plan Project</Button>
 									</Row>
 								</Form>
 							</Card.Body>
