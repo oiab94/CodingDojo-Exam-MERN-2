697daf64c659207cb4e9b1ccf62d3103a34bab77
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-02-27 05:10:52 -0300

    [server] Indica el acceso a la API de delete

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -1,10 +1,12 @@
 const {
 	setProject, 
 	getProjects,
-	updateProject } = require("../controllers/project.controller");
+	updateProject,
+	deleteProject } = require("../controllers/project.controller");
 
 module.exports = (app) => {
 	app.post("/api/setProject", setProject);
 	app.get("/api/getProjects", getProjects);
-	app.put("/api/updateProject/:id", updateProject)
+	app.put("/api/updateProject/:id", updateProject);
+	app.delete("/api/deleteProject/:id", deleteProject);
 }
