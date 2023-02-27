89e8ef1cfb3d0a6b0d1ff6c89de9341abd6de300
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-02-27 01:51:47 -0300

    [client] Modificado la forma de recibir los datos

--- client/src/components/CardKanban.jsx
+++ client/src/components/CardKanban.jsx
@@ -1,14 +1,20 @@
 import {Card, Button} from "react-bootstrap";
 
 const CardKanban = (props) => {
-	const {title, date, state, status} = props.values;
+	const {title, dueDate, state, status} = props.values;
+	const due = new Date(dueDate);
 
 	return (
 		<>
 			<Card className="mt-2 ">
 				<Card.Body>
 					<Card.Title>{ title }</Card.Title>
-					<Card.Text>{ date }</Card.Text>
+					<Card.Text>
+						Due: {
+							(due.getUTCMonth()+ 1) + " / " +
+							due.getUTCDate() + " / " +
+							due.getUTCFullYear() }
+						</Card.Text>
 					<Button className={state}>{ status }</Button>
 				</Card.Body>
 			</Card>
